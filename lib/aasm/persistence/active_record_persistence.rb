31b71bfa058c0eb940fc0ef178f83c3e7f5fa1e7
unabl4 <unabl4@gmail.com>
2013-01-08 16:13:55 +0200

    Update lib/aasm/persistence/active_record_persistence.rb

--- lib/aasm/persistence/active_record_persistence.rb
+++ lib/aasm/persistence/active_record_persistence.rb
@@ -36,7 +36,7 @@ module AASM
         base.extend AASM::Persistence::ActiveRecordPersistence::ClassMethods
         base.send(:include, AASM::Persistence::ActiveRecordPersistence::InstanceMethods)
         base.send(:include, AASM::Persistence::ReadState) unless base.method_defined?(:aasm_read_state)
-        base.send(:include, AASM::Persistence::ActiveRecordPersistence::WriteState) unless base.method_defined?(:aasm_write_stat
+        base.send(:include, AASM::Persistence::ActiveRecordPersistence::WriteState) unless base.method_defined?(:aasm_write_state)
         base.send(:include, AASM::Persistence::ActiveRecordPersistence::WriteStateWithoutPersistence) unless base.method_defined?(:aasm_write_state_without_persistence)
         base.before_create(:aasm_ensure_initial_state)
 
